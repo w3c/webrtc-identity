b47c60b8f4a4a0f56d6a925b949ec51f6053eb5f
Dominique Hazael-Massieux <dom@w3.org>
2021-07-15 14:42:46 +0200

    Upgrade respec version to deal with latest WebIDL support

--- identity.js
+++ identity.js
@@ -1,13 +1,7 @@
-// Working around  respec using WHATWG HTML links for EventHandler
-// See also https://github.com/w3c/respec/issues/1372
-function cleanHTMLRef() {
-    [...document.querySelectorAll("a[href='https://html.spec.whatwg.org/multipage/#eventhandler']")].forEach(a => a.href = "https://www.w3.org/TR/html52/webappapis.html#event-handler");
-}
-
 var respecConfig = {
   // specification status (e.g. WD, LCWD, NOTE, etc.). If in doubt use ED.
   specStatus:           "ED",
-
+  xref: ["html", "webidl", "webrtc", "file-api", "mediacapture-streams", "hr-time", "dom"],
   // the specification's short name, as in http://www.w3.org/TR/short-name/
   shortName:            "webrtc-identity",
   useExperimentalStyles: true,
@@ -18,9 +12,6 @@ var respecConfig = {
 
   // if you wish the publication date to be other than today, set this
   // publishDate:  "2014-01-27",
-
-  // new ability to override the copyright completely
-  overrideCopyright:  "<p class='copyright'>Initial Author of this Specification was Ian Hickson, Google Inc., with the following copyright statement:<br /> &#169; Copyright 2004-2011 Apple Computer, Inc., Mozilla Foundation, and Opera Software ASA. You are granted a license to use, reproduce and create derivative works of this document.</p> <p class='copyright'>All subsequent changes since 26 July 2011 done by the W3C WebRTC Working Group are under the following <a href='https://www.w3.org/Consortium/Legal/ipr-notice#Copyright'>Copyright</a>:<br />&#169; 2011-2017 <a href='https://www.w3.org/'><abbr title='World Wide Web Consortium'>W3C</abbr></a><sup>&#174;</sup> (<a href='https://www.csail.mit.edu/'><abbr title='Massachusetts Institute of Technology'>MIT</abbr></a>, <a href='https://www.ercim.eu/'><abbr title='European Research Consortium for Informatics and Mathematics'>ERCIM</abbr></a>, <a href='https://www.keio.ac.jp/'>Keio</a>, <a href='http://ev.buaa.edu.cn/'>Beihang<\/a>). <a href='https://www.w3.org/Consortium/Legal/copyright-documents'>Document use</a>  rules apply.</p> <p class='copyright'>For the entire publication on the W3C site the <a href='https://www.w3.org/Consortium/Legal/ipr-notice#Legal_Disclaimer'>liability</a> and <a href='https://www.w3.org/Consortium/Legal/ipr-notice#W3C_Trademarks'>trademark</a> rules apply.</p>",
 
 
   // if the specification's copyright date is a range of years, specify
@@ -62,122 +53,16 @@ var respecConfig = {
   //      company: "Your Company", companyURL: "http://example.com/" }
   //],
 
-  // name of the WG
-  wg:           "Web Real-Time Communications Working Group",
-
-  // URI of the public WG page
-  wgURI:        "https://www.w3.org/2011/04/webrtc/",
+  group:           "webrtc",
 
   // name (without the @w3c.org) of the public mailing to which comments are due
   wgPublicList: "public-webrtc",
 
-  // URI of the patent status for this WG, for Rec-track documents
-  // !!!! IMPORTANT !!!!
-  // This is important for Rec-track documents, do not copy a patent URI from a random
-  // document unless you know what you're doing. If in doubt ask your friendly neighbourhood
-  // Team Contact.
-  wgPatentURI:  "https://www.w3.org/2004/01/pp-impl/47318/status",
-  issueBase: "https://github.com/w3c/webrtc-identity/issues/",
-  testSuiteURI: "https://github.com/web-platform-tests/wpt/tree/master/webrtc",
-  otherLinks: [
-    {
-      key: "Participate",
-      data: [
-        {
-          value: "Mailing list",
-          href: "https://lists.w3.org/Archives/Public/public-webrtc/"
-        },
-        {
-          value: "Browse open issues",
-          href: "https://github.com/w3c/webrtc-identity/issues"
-        },
-        {
-          value: "IETF RTCWEB Working Group",
-          href: "https://tools.ietf.org/wg/rtcweb/"
-        }
-      ]
+  github: "https://github.com/w3c/webrtc-identity/",
+  testSuiteURI: "https://github.com/web-platform-tests/wpt/tree/master/webrtc-identity",
+  localBiblio:  {
+    "JSEP": {
+      "aliasOf": "RFC8829"
     }
-  ],
-  preProcess: [
-      function linkToJsep() {
-          require(["core/pubsubhub"], function(pubsubhub){
-              var xhr = new XMLHttpRequest();
-              xhr.open('GET', 'jsep-mapping/map.json');
-              xhr.onload = function(e) {
-                  var data = JSON.parse(this.responseText);
-                  if (respecConfig.localBiblio.JSEP.date !== data.metadata.date) {
-                      pubsubhub.pub("warn", "Date of JSEP draft in localBiblio (" + respecConfig.localBiblio.date + ") does not match date of JSEP draft used in map.json (" + data.metadata.date + ").");
-                  }
-                  // Replace all
-                  //    <span data-jsep="foo">[[!JSEP]]</a>
-                  // with
-                  //    <span>[[!JSEP]] (<a href="...#X.Y">section X.Y.</a>)
-                  // based on mapping maintained in jsep-mapping/map.json
-                  Array.prototype.forEach.call(
-                      document.querySelectorAll("*[data-jsep]"),
-                      function (el) {
-                          var jsepAnchors = el.dataset.jsep.split(" ");
-                          var jsepSections = jsepAnchors.map(function (s) { return data.sections["sec." + s] || data.sections["sec-" + s];});
-                          if (jsepSections.indexOf(undefined) !== -1) {
-                              pubsubhub.pub("warn", "Reference to inexistent JSEP section in '" + el.dataset.jsep + "': unrecognized anchor #" + jsepSections.indexOf(undefined) + " 'sec." + jsepAnchors[jsepSections.indexOf(undefined)] + "'.");
-                              return;
-                          }
-                          el.removeAttribute("data-jsep");
-                          el.appendChild(document.createTextNode(" ("));
-                          jsepSections.forEach(function (s, i) {
-                              var sectionLink = document.createElement("a");
-                              sectionLink.href = "https://tools.ietf.org/html/draft-ietf-rtcweb-jsep-" + data.metadata.version + "#section-" +  s.slice(0, s.length - 1);
-                              sectionLink.textContent = "section " + s;
-                              if (i > 0) {
-                                  if (i == jsepSections.length - 1) {
-                                      el.appendChild(document.createTextNode(" and "));
-                                  } else {
-                                      el.appendChild(document.createTextNode(", "));
-                                  }
-                              }
-                              el.appendChild(sectionLink);
-                          });
-                          el.appendChild(document.createTextNode(")"));
-                      });
-              };
-              xhr.send();
-          });
-      }
-  ],
-    afterEnd: function markFingerprinting () {
-        Array.prototype.forEach.call(
-            document.querySelectorAll(".fingerprint"),
-            function (el) {
-                var img = new Image();
-                img.src = "images/fingerprint.png";
-                img.alt = "(This is a fingerprinting vector.)";
-                img.width = 15;
-                img.height = 21;
-                el.appendChild(img);
-            });
-    },
-    localBiblio: {
-        "JSEP": {
-            "authors":["Justin Uberti","Cullen Jennings","Eric Rescorla"],
-            "href": "https://tools.ietf.org/html/draft-ietf-rtcweb-jsep/",
-            "publisher": "IETF",
-            "status": "Active Internet-Draft",
-            "title": "Javascript Session Establishment Protocol",
-            "date": "10 October 2017"
-        },
-        "STUN-PARAMETERS": {
-            "authors":["IETF"],
-            "href": "https://www.iana.org/assignments/stun-parameters/stun-parameters.xhtml#stun-parameters-6",
-            "publisher": "IANA",
-            "status": "IANA Parameter Assignment",
-            "title": "STUN Error Codes",
-            "date": "April 2011"
-        },
-        "IANA-HASH-FUNCTION": {
-            "href": "https://www.iana.org/assignments/hash-function-text-names/hash-function-text-names.xml",
-            "publisher": "IANA",
-            "title": "Hash Function Textual Names"
-        }
-    },
-    postProcess: [cleanHTMLRef]
+  }
 };
